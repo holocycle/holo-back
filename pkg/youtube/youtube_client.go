3ab07e35754d3ce5f4ef3c3b75bdbc3e711d15f4
somq14 <som.q14@gmail.com>
2020-05-16 14:52:17 +0900

    add: youtube api GetChannel

--- pkg/youtube/youtube_client.go
+++ pkg/youtube/youtube_client.go
@@ -5,12 +5,14 @@ import "github.com/holocycle/holo-back/pkg/model"
 type Config struct {
 	APIKey string `required:"true" env:"YOUTUBE_CLIENT_API_KEY"`
 	APIURL struct {
-		Video string `required:"true"`
+		Video   string `required:"true"`
+		Channel string `required:"true"`
 	}
 }
 
 type Client interface {
 	GetVideo(videoID string) (*model.Video, error)
+	GetChannel(channelID string) (*model.Channel, error)
 }
 
 type ClientImpl struct {
