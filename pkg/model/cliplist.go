266d28976a415dd2e6dd7f54f52e20d4527cfd55
somq14 <som.q14@gmail.com>
2020-06-07 19:17:04 +0900

    add: CliplistContain.Status

--- pkg/model/cliplist.go
+++ pkg/model/cliplist.go
@@ -2,27 +2,37 @@ package model
 
 import "time"
 
+type CliplistStatus string
+
+const (
+	CliplistStatusPublic  CliplistStatus = "PUBLIC"
+	CliplistStatusDeleted CliplistStatus = "DELETED"
+)
+
 type Cliplist struct {
 	ID          string
 	UserID      string
 	Title       string
 	Description string
+	Status      CliplistStatus
 	CreatedAt   *time.Time
 	UpdatedAt   *time.Time
 
-	Clip []*Clip
+	CliplistContains []*CliplistContain
 }
 
 func NewCliplist(
 	userID,
 	title,
 	description string,
+	status CliplistStatus,
 ) *Cliplist {
 	return &Cliplist{
 		ID:          NewID(),
 		UserID:      userID,
 		Title:       title,
 		Description: description,
+		Status:      status,
 		CreatedAt:   nil,
 		UpdatedAt:   nil,
 	}
