dd2c6ef616a62a87721f599b73810708f217101c
platykun <nagonago561@yahoo.co.jp>
2020-06-18 17:21:24 +0900

    add: 初期データ投入用(DDLではない)のスクリプトの準備

---
+++ data/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/holocycle/holo-back/internal/app/config"
+	"github.com/holocycle/holo-back/pkg/db"
+	"github.com/holocycle/holo-back/pkg/model"
+	"github.com/holocycle/holo-back/pkg/repository"
+)
+
+func main() {
+	config, err := config.NewConfig()
+	if err != nil {
+		fmt.Printf("Failed to load configuration. err=%+v\n", err)
+		return
+	}
+
+	db, err := db.NewDB(&config.DB)
+	defer db.Close()
+
+	userRepository := repository.NewUserRepository()
+
+	userRepository.NewQuery(db).Create(model.NewUser("user1", "test1@test.com", "https://yt3.ggpht.com/a/AATXAJwHPp_TkvcWJyblt9XVYDjNSjrj6KdpQSCQNQ=s288-c-k-c0xffffffff-no-rj-mo"))
+	userRepository.NewQuery(db).Create(model.NewUser("user2", "test2@test.com", "https://yt3.ggpht.com/a/AATXAJwHPp_TkvcWJyblt9XVYDjNSjrj6KdpQSCQNQ=s288-c-k-c0xffffffff-no-rj-mo"))
+}
